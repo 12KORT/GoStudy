2fa0ec01bbb8575cc24a3fc949a4614c5c7e279d
12KORT <tcwangfei@sina.com.cn>
2018-07-22 23:14:45 +0800

    add  read function

--- src/file/read.go
+++ src/file/read.go
@@ -1,7 +1,21 @@
 package filelib
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
 
 func Test() {
-	fmt.Println("test")
+
+}
+
+func ReadFrom(reader io.Reader, num int) ([]byte, error) {
+	p := make([]byte, num)
+	n, err := reader.Read(p)
+	if n > 0 {
+		return p[:n], nil
+	}
+	return p, err
 }
